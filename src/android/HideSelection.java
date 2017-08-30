85056608a2f11e94ecc135cb47ffdd8d316bd26a
Weverton Peron <wevertonperon@Wevertons-MacBook-Pro.local>
2017-08-30 18:03:28 -0300

    Fixes

--- src/android/HideSelection.java
+++ src/android/HideSelection.java
@@ -23,7 +23,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ImageView;
 
-public class HideSelection extends CordovaPlugin implements OnTouchListener {
+public class HideSelection extends CordovaPlugin {
 
     private CordovaWebView webView;
 
@@ -31,30 +31,51 @@ public class HideSelection extends CordovaPlugin implements OnTouchListener {
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
       super.initialize(cordova, webView);
       this.webView = webView;
-      mTextSelectionSupport = TextSelectionSupport.support(this, mWebView);
-      mTextSelectionSupport.setSelectionListener(new TextSelectionSupport.SelectionListener() {
-          @Override
-          public void startSelection() {
-          }
-          @Override
-          public void selectionChanged(String text) {
-              Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-          }
-          @Override
-          public void endSelection() {
-          }
-      });
-      mWebView.setWebViewClient(new WebViewClient() {
-          public void onScaleChanged(WebView view, float oldScale, float newScale) {
-              mTextSelectionSupport.onScaleChanged(oldScale, newScale);
-          }
-      });
+    }
+
+    @Override
+    public void onActionModeStarted(ActionMode mode) {
+        if (mActionMode == null) {
+            mActionMode = mode;
+            Menu menu = mode.getMenu();
+            menu.clear();
+            // getMenuInflater().inflate(R.menu.YOUR_MENU, menu);
+            // List<MenuItem> menuItems = new ArrayList<>();
+            // // get custom menu item
+            // for (int i = 0; i < menu.size(); i++) {
+            //     menuItems.add(menu.getItem(i));
+            // }
+            // menu.clear();
+            // // reset menu item order
+            // int size = menuItems.size();
+            // for (int i = 0; i < size; i++) {
+            //     addMenuItem(menu, menuItems.get(i), i, true);
+            // }
+            super.onActionModeStarted(mode);
+        }
+    }
+
+
+    /**
+     * add custom item to menu
+     * @param menu
+     * @param item
+     * @param order
+     * @param isClick
+     */
+    private void addMenuItem(Menu menu, MenuItem item, int order, boolean isClick){
+        MenuItem menuItem = menu.add(item.getGroupId(),
+                item.getItemId(),
+                order,
+                item.getTitle());
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        if (isClick)
+            // set custom menu item click
+            menuItem.setOnMenuItemClickListener(this);
     }
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-        this.
 
         if (action.equals("hideMenu")) {
 
@@ -70,55 +91,5 @@ public class HideSelection extends CordovaPlugin implements OnTouchListener {
 
         }
     }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        final Context ctx = mActivity;
-        float xPoint = getDensityIndependentValue(event.getX(), ctx) / getDensityIndependentValue(mScale, ctx);
-        float yPoint = getDensityIndependentValue(event.getY(), ctx) / getDensityIndependentValue(mScale, ctx);
-
-        switch (event.getAction()) {
-        case MotionEvent.ACTION_DOWN:
-            // Essential to add Locale.US parameter to String.format, else does not work on systems
-            // with default locale different, with other floating point notations, e.g. comma instead
-            // of decimal point.
-            final String startTouchUrl = String.format(Locale.US, "javascript:android.selection.startTouch(%f, %f);", xPoint, yPoint);
-            mLastTouchX = xPoint;
-            mLastTouchY = yPoint;
-            mWebView.loadUrl(startTouchUrl);
-            break;
-        case MotionEvent.ACTION_UP:
-            if (!mScrolling) {
-                endSelectionMode();
-                //
-                // Fixes 4.4 double selection
-                // See: http://stackoverflow.com/questions/20391783/how-to-avoid-default-selection-on-long-press-in-android-kitkat-4-4
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    return false;
-                }
-            }
-            mScrollDiffX = 0;
-            mScrollDiffY = 0;
-            mScrolling = false;
-            //
-            // Fixes 4.4 double selection
-            // See: http://stackoverflow.com/questions/20391783/how-to-avoid-default-selection-on-long-press-in-android-kitkat-4-4
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && isInSelectionMode()) {
-            	return true;
-            }
-            break;
-        case MotionEvent.ACTION_MOVE:
-            mScrollDiffX += (xPoint - mLastTouchX);
-            mScrollDiffY += (yPoint - mLastTouchY);
-            mLastTouchX = xPoint;
-            mLastTouchY = yPoint;
-            if (Math.abs(mScrollDiffX) > SCROLLING_THRESHOLD || Math.abs(mScrollDiffY) > SCROLLING_THRESHOLD) {
-                mScrolling = true;
-            }
-            break;
-        }
-        return false;
-    }
-
 
 }
