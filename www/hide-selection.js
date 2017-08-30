9b3987110aa3f64bdf4342fd6a7999404993e29e
Weverton Peron <wevertonperon@Wevertons-MacBook-Pro.local>
2017-08-30 12:21:47 -0300

    Improving plugin

--- www/hide-selection.js
+++ www/hide-selection.js
@@ -1,5 +1,6 @@
 module.exports = {
     text_selection: function (successCallback, errorCallback) {
+        alert(32);
         cordova.exec(successCallback, errorCallback, "HideSelection", "hideMenu", []);
     }
 };
