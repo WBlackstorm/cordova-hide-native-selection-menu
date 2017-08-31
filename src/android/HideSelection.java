0dbe49debfa7e985b759ca8702a309b7b5c61304
Weverton Peron <wevertonperon@Wevertons-MacBook-Pro.local>
2017-08-31 15:38:01 -0300

    Fixes

--- src/android/HideSelection.java
+++ src/android/HideSelection.java
@@ -4,6 +4,7 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -16,13 +17,16 @@ public class HideSelection extends CordovaPlugin {
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
       this.webView = webView;
       this.webView.getView().setOnCreateContextMenuListener(null);
-      super.initialize(cordova, this.webView);      
+      super.initialize(cordova, this.webView);
     }
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("hideMenu")) {
+
+            Log.i("Teste", "Passou");
+            this.webView.getView().setOnCreateContextMenuListener(null);
 
             String name = data.getString(0);
             String message = "Hello, " + name;
