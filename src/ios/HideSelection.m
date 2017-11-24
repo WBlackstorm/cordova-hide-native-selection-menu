85775ffb86439b73b9edb0171f37bebf5d3030cb
Weverton Peron <wevertonperon@Wevertons-MacBook-Pro.local>
2017-11-24 16:37:48 -0200

    Fix

--- src/ios/HideSelection.m
+++ src/ios/HideSelection.m
@@ -3,20 +3,34 @@
 
 @implementation HideSelection
 
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    UITouch *theTouch = [touches anyObject];
+- (void)hideMenu:(CDVInvokedUrlCommand *)command {
 
-    if ([theTouch tapCount] == 2  && [self becomeFirstResponder]) {
+    NSLog(@"Teste");
 
-        // selection management code goes here...
+    UIMenuController *theMenu = [UIMenuController sharedMenuController];
+        // CGRect selectionRect = CGRectMake (currentSelection.x, currentSelection.y, SIDE, SIDE);
+        // [theMenu setTargetRect:selectionRect inView:self];
+    [theMenu setMenuVisible:NO animated:YES];
 
-        // bring up edit menu.
-        UIMenuController *theMenu = [UIMenuController sharedMenuController];
-        CGRect selectionRect = CGRectMake (currentSelection.x, currentSelection.y, SIDE, SIDE);
-        [theMenu setTargetRect:selectionRect inView:self];
-        [theMenu setMenuVisible:YES animated:YES];
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
 
-    }
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
+
+//- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+//    UITouch *theTouch = [touches anyObject];
+//
+//    if ([theTouch tapCount] == 2  && [self becom]) {
+//
+//        // selection management code goes here...
+//
+//        // bring up edit menu.
+//        UIMenuController *theMenu = [UIMenuController sharedMenuController];
+//        CGRect selectionRect = CGRectMake (currentSelection.x, currentSelection.y, SIDE, SIDE);
+//        [theMenu setTargetRect:selectionRect inView:self];
+//        [theMenu setMenuVisible:YES animated:YES];
+//
+//    }
+//}
 
 @end
