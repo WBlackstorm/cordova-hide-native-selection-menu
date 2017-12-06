a621e2d7ab31454f4422e4a7612189e5082b64a0
Weverton Peron <wevertonperon@Wevertons-MacBook-Pro.local>
2017-12-06 15:35:48 -0200

    Fix to iOS Safari Browser

--- src/ios/HideSelection.m
+++ src/ios/HideSelection.m
@@ -5,32 +5,15 @@
 
 - (void)hideMenu:(CDVInvokedUrlCommand *)command {
 
-    NSLog(@"Teste");
+  UIMenuController *theMenu = [UIMenuController sharedMenuController];
 
-    UIMenuController *theMenu = [UIMenuController sharedMenuController];
-        // CGRect selectionRect = CGRectMake (currentSelection.x, currentSelection.y, SIDE, SIDE);
-        // [theMenu setTargetRect:selectionRect inView:self];
-    [theMenu setMenuVisible:NO animated:YES];
+  [UIMenuController sharedMenuController].menuVisible = NO;
 
-    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+  //[[UIMenuController sharedMenuController] setTargetRect:CGRectMake(0, 0, 1, 1) inView:self.extraView];
 
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+  CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+
+  [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
-
-//- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-//    UITouch *theTouch = [touches anyObject];
-//
-//    if ([theTouch tapCount] == 2  && [self becom]) {
-//
-//        // selection management code goes here...
-//
-//        // bring up edit menu.
-//        UIMenuController *theMenu = [UIMenuController sharedMenuController];
-//        CGRect selectionRect = CGRectMake (currentSelection.x, currentSelection.y, SIDE, SIDE);
-//        [theMenu setTargetRect:selectionRect inView:self];
-//        [theMenu setMenuVisible:YES animated:YES];
-//
-//    }
-//}
 
 @end
